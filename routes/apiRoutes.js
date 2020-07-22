e2f7515e93cb335de9b3ce51efdc9553b1a91c46
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-21 20:07:35 -0500

    html routes and api routes updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const store = require("../db/store");
+const note = require("../models/note");
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", (req, res) => {
-  store
+  note
     .getNotes()
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
 });
 
 router.post("/notes", (req, res) => {
-  store
+  note
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
@@ -18,7 +18,7 @@ router.post("/notes", (req, res) => {
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
-  store
+  note
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
